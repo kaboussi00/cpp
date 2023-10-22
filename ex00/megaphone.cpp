6819f4b388a7f2dc0336e99823508025135bee44
Kawtar Aboussi <kautar.abussi@gmail.com>
2023-10-22 13:37:19 +0100

    fix: error in Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,13 +5,13 @@
 int main(int ac, char **av) {
     std::string str;
     if (ac < 2) {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
      for (int i = 1; i < ac; i++) {
         str = av[i];
         for (int j = 0; str[j] != '\0'; j++) {
-            char upperStr = std::toupper(av[i][j]);
-            std::cout << upperStr;
+            char Str = std::toupper(str[j]);
+            std::cout << Str;
         }
     }
     std :: cout << "\n";
